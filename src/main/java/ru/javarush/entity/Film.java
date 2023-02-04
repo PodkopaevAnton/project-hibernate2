ad836acc940b474e433991e7f69fe329856b0fe5
Anton Podkopaev <test@email.com>
2023-02-05 01:20:57 +0300

    add_entities_2

--- src/main/java/ru/javarush/entity/Film.java
+++ src/main/java/ru/javarush/entity/Film.java
@@ -64,4 +64,129 @@ public class Film {
     inverseJoinColumns=@JoinColumn(name = "actor_id",referencedColumnName = "actor_id"))
     private Set<Actor> actors;
 
+    @ManyToMany
+    @JoinTable(name = "film_category",
+            joinColumns = @JoinColumn(name = "film_id",referencedColumnName = "film_id"),
+            inverseJoinColumns=@JoinColumn(name = "category_id",referencedColumnName = "category_id"))
+    private Set<Category> categories;
+
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Year getReleaseYear() {
+        return releaseYear;
+    }
+
+    public void setReleaseYear(Year releaseYear) {
+        this.releaseYear = releaseYear;
+    }
+
+    public Language getLanguageId() {
+        return languageId;
+    }
+
+    public void setLanguageId(Language languageId) {
+        this.languageId = languageId;
+    }
+
+    public Language getOriginalLanguageId() {
+        return originalLanguageId;
+    }
+
+    public void setOriginalLanguageId(Language originalLanguageId) {
+        this.originalLanguageId = originalLanguageId;
+    }
+
+    public Byte getRentalDuration() {
+        return rentalDuration;
+    }
+
+    public void setRentalDuration(Byte rentalDuration) {
+        this.rentalDuration = rentalDuration;
+    }
+
+    public BigDecimal getRentalRate() {
+        return rentalRate;
+    }
+
+    public void setRentalRate(BigDecimal rentalRate) {
+        this.rentalRate = rentalRate;
+    }
+
+    public Short getLength() {
+        return length;
+    }
+
+    public void setLength(Short length) {
+        this.length = length;
+    }
+
+    public BigDecimal getReplacementCost() {
+        return replacementCost;
+    }
+
+    public void setReplacementCost(BigDecimal replacementCost) {
+        this.replacementCost = replacementCost;
+    }
+
+    public Rating getRating() {
+        return rating;
+    }
+
+    public void setRating(Rating rating) {
+        this.rating = rating;
+    }
+
+    public String getSpecialFeatures() {
+        return specialFeatures;
+    }
+
+    public void setSpecialFeatures(String specialFeatures) {
+        this.specialFeatures = specialFeatures;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public Set<Actor> getActors() {
+        return actors;
+    }
+
+    public void setActors(Set<Actor> actors) {
+        this.actors = actors;
+    }
+
+    public Set<Category> getCategories() {
+        return categories;
+    }
+
+    public void setCategories(Set<Category> categories) {
+        this.categories = categories;
+    }
 }
--- src/main/java/ru/javarush/entity/FilmText.java
+++ src/main/java/ru/javarush/entity/FilmText.java
@@ -1,5 +1,58 @@
 package ru.javarush.entity;
 
-public class FilmText {
+import jakarta.persistence.*;
+import org.glassfish.jaxb.core.v2.TODO;
+import org.hibernate.annotations.UpdateTimestamp;
 
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "film_text",schema = "movie")
+public class FilmText {
+    @Id
+    @Column(name = "film_id")
+    private Short id;
+
+    //TODO: @Id
+    @OneToOne
+    @JoinColumn(name = "film_id")
+    private Film film;
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description",columnDefinition = "text")
+    private String description;
+
+    public Short getId() {
+        return id;
+    }
+
+    public void setId(Short id) {
+        this.id = id;
+    }
+
+    public Film getFilm() {
+        return film;
+    }
+
+    public void setFilm(Film film) {
+        this.film = film;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
